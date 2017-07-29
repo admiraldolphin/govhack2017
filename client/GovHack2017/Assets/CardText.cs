797d9d2da86b845b768a4789c43a650740bbe2b0
Jon Manning <jon@secretlab.com.au>
2017-07-29 22:31:18 +1000

    Gameplay layout; initial state ingest

---
+++ client/GovHack2017/Assets/CardText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class CardText : MonoBehaviour {
+
+    private void Update()
+    {
+        var text = GetComponent<Text>();
+
+        var card = GetComponentInParent<Card>();
+
+        if (text.text != card.text)
+        {
+            text.text = card.text;
+        }
+
+    }
+}
---
+++ client/GovHack2017/Assets/JSONTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class JSONTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var text = @"{""you"":0,""state"":{""state"":0,""players"":{""0"":{""name"":""Player 0"",""hand"":null,""score"":0},""2"":{""name"":""Player 2"",""hand"":null,""score"":0}},""clock"":0,""whose_turn"":0}}";
+        var state = JsonConvert.DeserializeObject<Response>(text);  //JsonUtility.FromJson<Response>(text);
+
+	}
+	
+	
+}
