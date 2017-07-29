7b264b20be19a481a6b1739816201fff1b9e7827
Josh Deprez <josh.deprez@gmail.com>
2017-07-29 18:12:10 +1000

    Thinking about cards

--- server/game/card.go
+++ server/game/card.go
@@ -2,12 +2,20 @@ package game
 
 // PersonCard models a game card.
 type PersonCard struct {
-	Name string `json:"name"`
-	// TODO
+	Name   string   `json:"name"`
+	Traits []*Trait `json:"traits"`
 }
 
 // ActionCard models a game card.
 type ActionCard struct {
-	Name string `json:"name"`
-	// TODO
+	Name   string `json:"name"`
+	Trait  *Trait `json:"trait"`
+	Played bool   `json:"played"`
+}
+
+// Trait is a condition which match person cards, and are
+// played by action cards.
+type Trait struct {
+	Name  string `json:"name"`
+	Death bool   `json:"death"`
 }
