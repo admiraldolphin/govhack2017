4b3bdecb27420eb9c296b755b00154f635759c78
Josh Deprez <josh.deprez@gmail.com>
2017-07-29 17:30:12 +1000

    Refactor cards into new file

---
+++ server/game/card.go
@@ -0,0 +1,13 @@
+package game
+
+// PersonCard models a game card.
+type PersonCard struct {
+	Name string `json:"name"`
+	// TODO
+}
+
+// ActionCard models a game card.
+type ActionCard struct {
+	Name string `json:"name"`
+	// TODO
+}
