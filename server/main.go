477e460d3dff4c3298f9aa02742f141e7fa2b672
Josh Deprez <josh.deprez@gmail.com>
2017-07-29 10:05:16 +1000

    Open types, start serving nothign

--- server/main.go
+++ server/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+)
+
+var (
+	gamePort = flag.Int("game_port", 23456, "Port for the game to listen on")
+	httpPort = flag.Int("http_port", 23480, "Port the webserver listens on")
+)
+
 func main() {
-    println("Hello, world")
+	_, err := net.Listen("tcp", fmt.Sprintf(":%d", *gamePort))
+	if err != nil {
+		log.Fatalf("Couldn't listen on game port: %v", err)
+	}
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), nil); err != nil {
+		log.Fatalf("Couldn't serve HTTP: %v", err)
+	}
 }
