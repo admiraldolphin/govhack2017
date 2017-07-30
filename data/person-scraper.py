81a6b6688f1ec349ae478236bdb085bc579ed8c2
Sebastian Cook <git@tsuite.net>
2017-07-30 15:30:42 +1000

    Filtered person list to plausible persons.

---
+++ data/person-scraper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import json
+import os
+with open('person-original.json') as data_file:    
+    data = json.load(data_file)
+d = {}
+i = []
+j = 0
+print len(data)
+for person in data:
+	if 'year' in person['inquest']:
+		death_year = int(person['inquest']['year'])
+	else:
+		continue
+	if 'birth' in person and 'year' in person['birth']:
+		start_year = int(person['birth']['year'])
+	elif 'convict' in person and 'year' in person['convict']:
+		start_year = int(person['convict']['year'])
+	elif 'immigration' in person:
+		start_year = int(person['immigration']['year'])
+	else:
+		continue	
+	if death_year >= start_year:
+		continue
+	if 'marriage' in person and not (start_year >= int(person['marriage']['year']  >= death_year)):
+		continue
+	if 'bankruptcy' in person and not (start_year >= int(person['bankruptcy']['year']  >= death_year)):
+		continue
+	if 'census' in person and not (start_year >= int(person['census']['year']  >= death_year)):
+		continue
+	if 'health-welfare' in person and not (start_year >= int(person['health-welfare']['year']  >= death_year)):
+		continue
+	j = j+1
+	i.append(person)
+print j
+
+with open('person.json', 'w') as outfile:
+    json.dump(i, outfile, indent=4)		
