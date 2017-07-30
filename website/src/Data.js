336b31457547b089b9055e15df0bb5dc5ac3e2cd
Shea Bunge <bungeshea@gmail.com>
2017-07-30 16:22:03 +1000

    Fix broken reference permalinks

--- website/src/Data.js
+++ website/src/Data.js
@@ -14,13 +14,13 @@ class Data extends Component {
         {this.props.data.birth.birth_date &&
         <p>
           They were born on {this.props.data.birth.birth_date} at {this.props.data.birth.birth_place} to {this.props.data.birth.birth_mother} and {this.props.data.birth.birth_father}.
-          <cite><a href={this.props.data.convict.permalink} title="View reference" target="_blank">ref</a></cite>
+          <cite><a href={this.props.data.birth.permalink} title="View reference" target="_blank">ref</a></cite>
         </p>
         }
         {this.props.data.immigration.from_country &&
         <p>
           They immigrated from {this.props.data.immigration.from_country} in {this.props.data.immigration.year}.
-          <cite><a href={this.props.data.convict.permalink} title="View reference" target="_blank">ref</a></cite>
+          <cite><a href={this.props.data.immigration.permalink} title="View reference" target="_blank">ref</a></cite>
         </p>
         }
         {this.props.data.convict.convict_port &&
@@ -32,7 +32,7 @@ class Data extends Component {
         {this.props.data.bankruptcy.bankrupt_date &&
         <p>
           They were declared bankrupt on {this.props.data.bankruptcy.bankrupt_date}.
-          <cite><a href={this.props.data.convict.permalink} title="View reference" target="_blank">ref</a></cite>
+          <cite><a href={this.props.data.bankruptcy.permalink} title="View reference" target="_blank">ref</a></cite>
         </p>
         }
         {this.props.data.marriage.marriage_date &&
@@ -44,19 +44,19 @@ class Data extends Component {
         {this.props.data.court.trial_offence &&
         <p>
           They went to court for {this.props.data.court.trial_offence} in {this.props.data.court.year}.
-          <cite><a href={this.props.data.convict.permalink} title="View reference" target="_blank">ref</a></cite>
+          <cite><a href={this.props.data.marriage.permalink} title="View reference" target="_blank">ref</a></cite>
         </p>
         }
         {this.props.data.census.year &&
         <p>
           They were on the {this.props.data.census.year} census.
-          <cite><a href={this.props.data.convict.permalink} title="View reference" target="_blank">ref</a></cite>
+          <cite><a href={this.props.data.census.permalink} title="View reference" target="_blank">ref</a></cite>
         </p>
         }
         {this.props.data.inquest.death_verdict &&
         <p>
           {this.props.data.inquest.death_verdict} on {this.props.data.inquest.death_date}.
-          <cite><a href={this.props.data.convict.permalink} title="View reference" target="_blank">ref</a></cite>
+          <cite><a href={this.props.data.inquest.permalink} title="View reference" target="_blank">ref</a></cite>
         </p>
         }
 
