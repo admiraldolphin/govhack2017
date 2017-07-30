94cfd8fa93e6f0f343dd53e0882b75baa882205c
Shea Bunge <bungeshea@gmail.com>
2017-07-30 13:01:44 +1000

    Fix background layers being hidden

--- website/src/Data.js
+++ website/src/Data.js
@@ -4,25 +4,27 @@ import './App.css';
 class Data extends Component {
   render() {
     return (
-      <div className="data">
-        <h1>
-          You killed {this.props.data.NAME}!
-        </h1>
-        {this.props.data.DEATH_DATE &&
-          <p>
-            Date of DEATH: {this.props.data.DEATH_DATE}
-          </p>
-        }
-        {this.props.data.AGE &&
-        <p>
-          Aged {this.props.data.AGE}
-        </p>
-        }
-        {this.props.data.VERDICT &&
-        <p>
-          {this.props.data.VERDICT}
-        </p>
-        }
+      <div className="main">
+          <div className="data">
+            <h1>
+              You killed {this.props.data.NAME}!
+            </h1>
+            {this.props.data.DEATH_DATE &&
+              <p>
+                Date of DEATH: {this.props.data.DEATH_DATE}
+              </p>
+            }
+            {this.props.data.AGE &&
+            <p>
+              Aged {this.props.data.AGE}
+            </p>
+            }
+            {this.props.data.VERDICT &&
+            <p>
+              {this.props.data.VERDICT}
+            </p>
+            }
+          </div>
       </div>
     );
   }
