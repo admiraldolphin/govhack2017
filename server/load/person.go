b1c0a090a1447f7722eebf854c53a45c49639b69
Josh Deprez <josh.deprez@gmail.com>
2017-07-30 10:32:33 +1000

    Fix crash temporarily for Jon

--- server/load/person.go
+++ server/load/person.go
@@ -4,63 +4,63 @@ package load
 type Person struct {
 	Name    string `json:"name"`
 	Inquest struct {
-		DeathDate    string   `json:"death_date"`
-		DeathVerdict string   `json:"death_verdict"`
-		DeathCauses  []string `json:"death_causes"`
-		Year         string   `json:"year"`
-		Permalink    string   `json:"permalink"`
-	} `json:"inquest"`
+		DeathDate    string   `json:"death_date,omitempty"`
+		DeathVerdict string   `json:"death_verdict,omitempty"`
+		DeathCauses  []string `json:"death_causes,omitempty"`
+		Year         string   `json:"year,omitempty"`
+		Permalink    string   `json:"permalink,omitempty"`
+	} `json:"inquest,omitempty"`
 	Birth struct {
-		BirthDate   string `json:"birth_date"`
-		BirthPlace  string `json:"birth_place"`
-		BirthMother string `json:"birth_mother"`
-		BirthFather string `json:"birth_father"`
-		Year        string `json:"year"`
-		Permalink   string `json:"permalink"`
-	} `json:"birth"`
+		BirthDate   string `json:"birth_date,omitempty"`
+		BirthPlace  string `json:"birth_place,omitempty"`
+		BirthMother string `json:"birth_mother,omitempty"`
+		BirthFather string `json:"birth_father,omitempty"`
+		Year        string `json:"year,omitempty"`
+		Permalink   string `json:"permalink,omitempty"`
+	} `json:"birth,omitempty"`
 	Immigration struct {
-		ImmigrationDate string `json:"immigration_date"`
-		FromCountry     string `json:"from_country"`
-		Year            string `json:"year"`
-		Permalink       string `json:"permalink"`
-	} `json:"immigration"`
+		ImmigrationDate string `json:"immigration_date,omitempty"`
+		FromCountry     string `json:"from_country,omitempty"`
+		Year            string `json:"year,omitempty"`
+		Permalink       string `json:"permalink,omitempty"`
+	} `json:"immigration,omitempty"`
 	Convict struct {
-		DepartureDate string `json:"departure_date"`
-		ConvictPort   string `json:"convict_port"`
-		ConvictShip   string `json:"convict_ship"`
-		Year          string `json:"year"`
-		Permalink     string `json:"permalink"`
-	} `json:"convict"`
+		DepartureDate string `json:"departure_date,omitempty"`
+		ConvictPort   string `json:"convict_port,omitempty"`
+		ConvictShip   string `json:"convict_ship,omitempty"`
+		Year          string `json:"yea,omitemptyr"`
+		Permalink     string `json:"permalink,omitempty"`
+	} `json:"convict,omitempty"`
 	Bankruptcy struct {
-		BankruptDate string `json:"bankrupt_date"`
-		Year         string `json:"year"`
-		Permalink    string `json:"permalink"`
-	} `json:"bankruptcy"`
+		BankruptDate string `json:"bankrupt_date,omitempty"`
+		Year         string `json:"year,omitempty"`
+		Permalink    string `json:"permalink,omitempty"`
+	} `json:"bankruptcy,omitempty"`
 	Marriage struct {
-		MarriageDate  string `json:"marriage_date"`
-		SpouseName    string `json:"spouse_name"`
-		MarriagePlace string `json:"marriage_place"`
-		Year          string `json:"year"`
-		Permalink     string `json:"permalink"`
-	} `json:"marriage"`
+		MarriageDate  string `json:"marriage_date,omitempty"`
+		SpouseName    string `json:"spouse_name,omitempty"`
+		MarriagePlace string `json:"marriage_place,omitempty"`
+		Year          string `json:"year,omitempty"`
+		Permalink     string `json:"permalink,omitempty"`
+	} `json:"marriage,omitempty"`
 	Court struct {
-		TrialDate    string `json:"trial_date"`
-		TrialOffence string `json:"trial_offence"`
-		Year         string `json:"year"`
-		Permalink    string `json:"permalink"`
-	} `json:"court"`
+		TrialDate    string `json:"trial_date,omitempty"`
+		TrialOffence string `json:"trial_offence,omitempty"`
+		Year         string `json:"year,omitempty"`
+		Permalink    string `json:"permalink,omitempty"`
+	} `json:"court,omitempty"`
 	HealthWelfare struct {
-		AdmissionDate string `json:"admission_date"`
-		Property      string `json:"property"`
-		Remarks       string `json:"remarks"`
-		Year          string `json:"year"`
-		Permalink     string `json:"permalink"`
-	} `json:"health-welfare"`
+		AdmissionDate string `json:"admission_date,omitempty"`
+		Property      string `json:"property,omitempty"`
+		Remarks       string `json:"remarks,omitempty"`
+		Year          string `json:"year,omitempty"`
+		Permalink     string `json:"permalink,omitempty"`
+	} `json:"health-welfare,omitempty"`
 	Census struct {
-		CensusYear     string `json:"census_year"`
-		CensusPlace    string `json:"census_place"`
-		CensusChildren bool   `json:"census_children"`
-		Year           string `json:"year"`
-		Permalink      string `json:"permalink"`
-	} `json:"census"`
+		CensusYear     string `json:"census_year,omitempty"`
+		CensusPlace    string `json:"census_place,omitempty"`
+		CensusChildren bool   `json:"census_children,omitempty"`
+		Year           string `json:"year,omitempty"`
+		Permalink      string `json:"permalink,omitempty"`
+	} `json:"census,omitempty"`
 }
